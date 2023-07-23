972befebdc93eb521a3952219394ae303b8f355d
MiguelAngMolina <miguel11881@gmail.com>
2023-07-23 14:22:22 -0500

    Card2

--- src/main/java/com/example/foodcompare_vm/CardController.java
+++ src/main/java/com/example/foodcompare_vm/CardController.java
@@ -1,4 +1,32 @@
 package com.example.foodcompare_vm;
 
+import com.example.model.Food;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class CardController {
+
+    @FXML
+    private Button box;
+
+    @FXML
+    private Label foodName;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Label price;
+
+    @FXML
+    private Label restaurant;
+
+
+    public void setData(Food food ){
+        Image image = new Image(getClass().getResource())
+    }
+
 }
