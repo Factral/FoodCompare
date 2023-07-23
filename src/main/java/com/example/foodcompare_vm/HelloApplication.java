e63fd3c1cd7c4475167c55e0548afaffd6be55b5
MiguelAngMolina <miguel11881@gmail.com>
2023-07-23 11:05:10 -0500

    Prueba MenuPrincipal

--- src/main/java/com/example/foodcompare_vm/HelloApplication.java
+++ src/main/java/com/example/foodcompare_vm/HelloApplication.java
@@ -2,22 +2,29 @@ package com.example.foodcompare_vm;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Menú_Pp.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
-        stage.setTitle("messi!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        // Cargar el archivo FXML
+        Parent root = FXMLLoader.load(getClass().getResource("Menú_Pp.fxml"));
+
+        // Crear la Scene con el contenido cargado desde FXML
+        Scene scene = new Scene(root); // Ajusta el tamaño según tus necesidades
+
+        // Configurar el Stage (ventana principal)
+        primaryStage.setTitle("Aplicación JavaFX");
+        primaryStage.setScene(scene);
+
+        // Mostrar la ventana
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
